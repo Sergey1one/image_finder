1f30a1fe5535fbfe3022362769fd7a6d96cad6f2
Sergey1one <sm.bydin@gmail.com>
2023-12-14 11:52:09 +0200

    NC

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { App2 } from 'components/App2';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
